e2701d3ed3906c5166315b9df9abfd073c62af87
Håkon Legernæs <haakoleg@tutanota.com>
2018-09-11 18:10:48 +0200

    Return 404 for invalid paths

--- main.go
+++ main.go
@@ -5,10 +5,32 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
 )
 
-func mainHandler(w http.ResponseWriter, r *http.Request) {
+const rootPath = "igcinfo"
 
+func mainHandler(w http.ResponseWriter, r *http.Request) {
+	// Get request paths
+	paths := strings.Split(r.URL.Path[1:], "/")
+
+	fmt.Printf("Paths len: %d\n", len(paths))
+	fmt.Println(paths)
+
+	// If root path is not igcinfo, next path not api, reply with 404
+	if len(paths) != 2 || paths[0] != rootPath || paths[1] != "api" {
+		http.NotFound(w, r)
+		return
+	}
+
+	handleRoutes(&w, r, paths[2:])
+}
+
+func handleRoutes(w *http.ResponseWriter, r *http.Request, routes []string) {
+	// Base api route
+	if len(routes) == 0 {
+
+	}
 }
 
 func main() {
